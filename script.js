01239993d1ce40834713a926c068926d9547caf2
Potency <proper@ProperPoe>
2023-08-12 12:50:14 -0600

    Changed query selectors on the buttons, provided logic to evaluate the operators, and render them on the screen
    Instead of querySelectAll on all button elements, I have different id selectors to the numbers and operators. With this, I created three empty arrays to store the first number, the operator, and second number. I added event listeners to each operand and operator when clicked, and made two more arrays that show the result and evaluation process in the display screen on the calculator. A boolean flag was made to check the condition if the operator was clicked or not. I also used css to position the elements in the display screen

--- script.js
+++ script.js
@@ -1,29 +1,65 @@
-const calcBtn = document.querySelectorAll("button");
+const operand = document.querySelectorAll(".operand");
+const operator = document.querySelectorAll(".operator");
+const calcBtn = document.querySelectorAll("button")
+const display = document.getElementById("calc-display")
+const resultDisplay = document.getElementById("result-display")
+const calculations = document.getElementById("calculations")
 
+let numOne = [];
+let numTwo = [];
+let operators = [];
+let result = [];
+let operationsArray = []
 
-calcBtn.forEach((operand) => {
-    operand.addEventListener("click", ()=>{
-        calculation(operand)
-        add()
-        subtract()
-        multiply()
-        divide()
+let operatorNotClick = true;
+
+operand.forEach((sum) => {
+    sum.addEventListener("click", () => {
+        if(operatorNotClick){
+            numOne.push(+sum.value)
+            operationsArray.push(+sum.value)
+            calculations.innerText = operationsArray.join("")
+        }else{
+            numTwo.push(+sum.value)
+            operationsArray.push(+sum.value)
+            calculations.innerText = operationsArray.join("")
+        }
+
+        operator.forEach((op) => {
+            op.addEventListener("click", () => {
+                operatorNotClick = false
+                operators.push(op.value)
+                console.log(operators)
+                console.log(operators[0])
+                if(operators.length < 2){
+                    operationsArray.push(operators[0])
+                }
+                calculations.innerText = operationsArray.join("")
+                calc(operators[0])
+            })
+        
+            
+        })
+        let num1 = numOne.join("")
+        let num2 = numTwo.join("")
+
+        function calc (hmm){
+            if(+num1 > 0 && +num2 > 0){
+                if(hmm === "+"){
+                    result.push(+num1 + +num2)
+                }else if(hmm === "-"){
+                    result.push(+num1 - +num2)
+                }else if(hmm === "x"){
+                    result.push(+num1 * +num2)
+                }else if(hmm === "/"){
+                    result.push(+num1 / +num2)
+                }
+                resultDisplay.innerText = result[0]
+            }
+            console.log(operationsArray)
+        } 
     })
 })
 
-const add = (operator) => {
-    
-}
-const subtract = (operator) => {
-    
-}
-const multiply = (operator) => {
-    
-}
-const divide = (operator) => {
-    
-}
 
-const calculation = (value) => {
-    console.log(value.value)
-}
+
