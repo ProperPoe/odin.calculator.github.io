9e7f905531b632bf5b1472745fff96b8043edbdf
Potency <proper@ProperPoe>
2023-08-12 05:51:52 -0600

    Created add, subtract, multiple, divide functions

--- script.js
+++ script.js
@@ -4,8 +4,25 @@ const calcBtn = document.querySelectorAll("button");
 calcBtn.forEach((operand) => {
     operand.addEventListener("click", ()=>{
         calculation(operand)
+        add()
+        subtract()
+        multiply()
+        divide()
     })
 })
+
+const add = (operator) => {
+    
+}
+const subtract = (operator) => {
+    
+}
+const multiply = (operator) => {
+    
+}
+const divide = (operator) => {
+    
+}
 
 const calculation = (value) => {
     console.log(value.value)
