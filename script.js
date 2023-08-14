90cb3d39be4ddfc414f558ececa5352c86127c0e
Potency <proper@ProperPoe>
2023-08-14 07:42:54 -0600

    put the back button, equals button, and clear button into individual addEventListeners, so the values of each element dont't interact with the array
    provided more styling to the calculator so that the overflow is hidden if screen is overflowed. Put the back button into an individual addEventListener outside the forEach, so that it only pops one element of the array at a time

--- script.js
+++ script.js
@@ -1,51 +1,44 @@
 const operand = document.querySelectorAll(".operand");
-const operator = document.querySelectorAll(".operator");
-const calcBtn = document.querySelectorAll("button")
-const display = document.getElementById("calc-display")
 const resultDisplay = document.getElementById("result-display")
 const calculations = document.getElementById("calculations")
 const equals = document.getElementById("equals")
 const clear = document.getElementById("clear")
 const back = document.getElementById("back")
 
-let numOne = [];
-let numTwo = [];
-let operators = [];
-let result = [];
-let operationsArray = []
 let evalArray = []
-
-let operatorNotClick = true;
-let itsNotOver = true;
-
 
 
 const calc = (button) => {
     console.log(button.value)
     evalArray.push(button.value)
-    calculations.innerText = evalArray.filter((x)=>x!=="=").join("")
+    calculations.innerText = evalArray.join("")
     
-    if(button.value === "="){
+    
+    equals.addEventListener("click", () => {
         calculations.innerText = ""
-        let evalConnect = evalArray.slice(0,-1).join("")
-        //console.log(eval(evalConnect))
+        let evalConnect = evalArray.join("")
         resultDisplay.innerText = eval(evalConnect)
-    }
-    if(button.value === "clear"){
+    })
+
+    clear.addEventListener("click", () => {
         evalArray.length = 0
         resultDisplay.innerText = ""
         calculations.innerText = ""
-    }
-    /*if(button.value === "back"){
-        evalArray.pop()
-        console.log(evalArray.pop())
-        //calculations.innerText = evalArray.pop().join("")
-    }*/
+    })
+
 }
+
+back.addEventListener("click", () => {
+    if (evalArray.length > 0) {
+        evalArray.pop();
+        calculations.innerText = evalArray.join("");
+    }
+});
 
 operand.forEach((button) => {
     button.addEventListener(("click"), () => {
         calc(button)
+       
     })
 })
 
